ffcba9a4f23e816dbc3ba77cc66b2cc72e686d96
LocTD <it.tranducloc@gmail.com>
2021-12-27 14:47:53 +0700

    Update fzf env

--- fzf/env.zsh
+++ fzf/env.zsh
@@ -1,1 +1,2 @@
 export FZF_CTRL_T_OPTS="--bind '?:toggle-preview' --preview 'bat --color=always --style=numbers --line-range=:500 {}' --select-1 --exit-0"
+export FZF_DEFAULT_OPTS="--height=40% --layout=reverse --info=inline"
