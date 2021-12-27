e5730e340b5e7a68163fa225e158d03e9c9149ec
LocTD <it.tranducloc@gmail.com>
2021-12-27 14:47:18 +0700

    Update bat scripts

--- bat/install.sh
+++ bat/install.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/env bash
-echo "Install bat"
+echo 'Install bat'
 cd /tmp
-curl -LO https://github.com/sharkdp/bat/releases/download/v0.18.3/bat_0.18.3_amd64.deb
-dpkg -i ./bat_0.18.3_amd64.deb
+VERSION=0.18.3
+curl -LO "https://github.com/sharkdp/bat/releases/download/v${VERSION}/bat_${VERSION}_amd64.deb"
+dpkg -i "./bat_${VERSION}_amd64.deb"
+rm -f "./bat_${VERSION}_amd64.deb"
 cd -
--- bat/update.sh
+++ bat/update.sh
@@ -1,6 +1,7 @@
-echo 'update bat'
+echo 'Update bat'
 cd /tmp
 VERSION=0.18.3
 curl -LO "https://github.com/sharkdp/bat/releases/download/v${VERSION}/bat_${VERSION}_amd64.deb"
 dpkg -i "./bat_${VERSION}_amd64.deb"
+rm -f "./bat_${VERSION}_amd64.deb"
 cd -
