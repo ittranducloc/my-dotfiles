70739b5614e84983fdc1ecfcc14dec8b390c191e
LocTD <it.tranducloc@gmail.com>
2021-12-28 08:25:24 +0700

    Add 'diff-so-fancy' tool

---
+++ diff-so-fancy/install.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/so-fancy/diff-so-fancy.git "$HOME/.diff-so-fancy"
---
+++ diff-so-fancy/update.sh
@@ -0,0 +1,4 @@
+echo 'Update diff-so-fancy'
+cd "$HOME/.diff-so-fancy" 2>&1 > /dev/null
+git pull
+cd - 2>&1 > /dev/null
