604258f3dbe005f51bc2d820ca9c9cf6a14e376d
LocTD <it.tranducloc@gmail.com>
2021-12-28 14:41:42 +0700

    Define system alias

--- system/alias.zsh
+++ system/alias.zsh
@@ -1,2 +1,7 @@
 alias ezsh='vi ~/.zshrc'
 alias rzsh='source ~/.zshrc'
+alias csh='function _csh(){touch $1; chmod +x $1; vi $1}; _csh'
+alias alrg='function _alrg(){alias | rg $1}; _alrg'
+alias alf='alias | fzf'
+alias agf='function _agf(){apt search $1 | rg -C 3 "^$1.*/focal"}; _agf'
+alias vif='vi $(fzf -m)'
