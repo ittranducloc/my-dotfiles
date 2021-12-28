52a1ea2d2a2eb031c1937b10844d6bbb6effcbb7
LocTD <it.tranducloc@gmail.com>
2021-12-28 09:09:41 +0700

    Add tig tool for browsing git repository

---
+++ tig/install.sh
@@ -0,0 +1,16 @@
+echo 'Install tig'
+
+echo 'Install requirement'
+apt-get install -y make gcc libncurses-dev
+CURRENT_DIR=$(pwd)
+cd /tmp
+VERSION=tig-2.5.4
+curl -LO https://github.com/jonas/tig/releases/download/"$VERSION"/"$VERSION".tar.gz
+tar xvf "$VERSION".tar.gz
+cd "$VERSION"
+make
+make install
+[ ! -f ~/bin/tig ] && echo 'Fail to install tig' && exit 1
+echo 'Succeed to install tig'
+$HOME/bin/tig --version
+cd $CURRENT_DIR
---
+++ tig/update.sh
@@ -0,0 +1,15 @@
+echo 'Update tig'
+
+CURRENT_DIR=$(pwd)
+cd /tmp
+OLD_VERSION=tig-2.5.4
+NEW_VERSION=tig-2.5.4
+curl -LO https://github.com/jonas/tig/releases/download/"$NEW_VERSION"/"$NEW_VERSION".tar.gz
+tar xvf "$NEW_VERSION".tar.gz
+cd "$NEW_VERSION"
+make
+make install
+[ ! -f ~/bin/tig ] && echo 'Fail to update tig' && exit 1
+echo "Succeed to update tig from $OLD_VERSION to $NEW_VERSION"
+$HOME/bin/tig --version
+cd $CURRENT_DIR
