27acaf84bb088297d84f07259718e965bb9594b1
LocTD <it.tranducloc@gmail.com>
2022-01-02 17:36:07 +0700

    Install 'rust' language by 'asdf' version manager

---
+++ asdf/rust_install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo '=========Install rust by asdf version manager'
+source ~/.asdf/asdf.sh
+asdf plugin add rust https://github.com/code-lever/asdf-rust.git
+asdf install rust 1.57.0
+asdf global rust 1.57.0
+[ ! -x $(command -v cargo) ] && echo 'Fail to install rust' && exit 1
+cargo --version
---
+++ rust/path.zsh
@@ -0,0 +1,1 @@
+export PATH=$PATH:$HOME/.asdf/installs/rust/1.57.0/bin
