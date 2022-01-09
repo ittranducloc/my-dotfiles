f532e85da392a0a74cbb9458f4114d1b37647702
LocTD <it.tranducloc@gmail.com>
2022-01-09 08:26:33 +0700

    Install neovim with latest version from github

---
+++ neovim/install.sh
@@ -0,0 +1,22 @@
+echo '=========Install neovim'
+echo ''
+apt-get update
+apt-get install -y software-properties-common python3 python3-distutils python3-dev python3-pip
+add-apt-repository -y ppa:neovim-ppa/stable
+apt-get update
+apt-get install -y neovim
+
+echo 'Installed version of neovim'
+nvim --version
+echo ''
+
+echo 'Use NeoVim for some (or all) of the editor alternatives'
+update-alternatives --remove-all vim
+update-alternatives --install "/usr/bin/vim" "vim" "/usr/bin/nvim" 1
+update-alternatives --remove-all vi
+update-alternatives --install "/usr/bin/vi" "vi" "/usr/bin/nvim" 1
+update-alternatives --remove-all editor
+update-alternatives --install "/usr/bin/editor" "editor" "/usr/bin/nvim" 1
+
+echo 'default vim/vi'
+update-alternatives --list vim
---
+++ neovim/update.sh
@@ -0,0 +1,8 @@
+echo '=========Update neovim'
+echo ''
+echo 'Current version of neovim'
+nvim --version
+apt-get update
+apt-install -y neovim
+echo 'Updated to latest version of neovim'
+nvim --version
