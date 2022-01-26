0d9656019b64405a75cdd07bb9c23fa18fc9fc27
LocTD <it.tranducloc@gmail.com>
2022-01-26 07:55:11 +0700

    Not auto create '.nvim' dir, just reference to custom config file

--- neovim/install.sh
+++ neovim/install.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 echo '=========Install neovim'
 echo ''
 apt-get update
@@ -11,18 +12,21 @@ nvim --version
 echo ''
 
 echo 'Use NeoVim for some (or all) of the editor alternatives'
-update-alternatives --remove-all vim
-update-alternatives --install "/usr/bin/vim" "vim" "/usr/bin/nvim" 1
-update-alternatives --remove-all vi
-update-alternatives --install "/usr/bin/vi" "vi" "/usr/bin/nvim" 1
 update-alternatives --remove-all editor
 update-alternatives --install "/usr/bin/editor" "editor" "/usr/bin/nvim" 1
 
-echo 'default vim/vi'
-update-alternatives --list vim
+echo 'default editor'
+update-alternatives --list editor
 
+# Just symbol link when the config file is not existed
 echo ''
 NVIMRC_REALPATH=$(realpath $(find . -iname 'init.vim'))
-echo "symlink config file"
-[ ! -d $HOME/.config/nvim ] && mkdir -p $HOME/.config/nvim
-ln -sf $NVIMRC_REALPATH $HOME/.config/nvim/init.vim
+NVIMRC_DEFAULT_PATH=$HOME/.config/nvim/init.vim
+if [[ ! -f $NVIMRC_DEFAULT_PATH ]]
+then
+	echo "Try to symlink config file"
+	ln -s $NVIMRC_REALPATH $NVIMRC_DEFAULT_PATH
+else
+	echo "" >> $NVIMRC_DEFAULT_PATH
+	echo "source $NVIMRC_REALPATH" >> $NVIMRC_DEFAULT_PATH
+fi
