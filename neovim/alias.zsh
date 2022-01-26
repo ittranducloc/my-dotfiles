0cb1ca17003c3caa7db1a41ab887d79d0a3771bc
LocTD <it.tranducloc@gmail.com>
2022-01-26 08:11:29 +0700

    Rename & add new alias for nvim to use vim session & neovim remote

--- neovim/alias.zsh
+++ neovim/alias.zsh
@@ -1,2 +1,14 @@
-alias vip='vi -p'
-alias vi='vi -O'
+alias vi=nvim
+alias vo='nvim -O'
+alias vp='nvim -p'
+alias vs='nvim -S'
+
+# https://github.com/nelstrom/modvim-code/blob/master/terminal/alias-nvim-nvr.sh
+# Prevent accidental nesting nvim instance inside terminal buffer
+if [ -n "$NVIM_LISTEN_ADDRESS" ]; then
+  if [ -x "$(command -v nvr)" ]; then
+    alias nvim=nvr
+  else
+    alias nvim='echo "No nesting!"'
+  fi
+fi
