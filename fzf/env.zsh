46b61236ec95442042021536ae178feb28d5c0c6
LocTD <it.tranducloc@gmail.com>
2022-02-08 23:28:05 +0700

    Check existence of `fd` instead of `rg`

--- fzf/env.zsh
+++ fzf/env.zsh
@@ -1,6 +1,6 @@
 export FZF_CTRL_T_OPTS="--bind '?:toggle-preview' --preview 'bat --color=always --style=numbers --line-range=:500 {}' --select-1 --exit-0"
 export FZF_DEFAULT_OPTS="--height=40% --layout=reverse --info=inline --ansi"
-if type rg &>/dev/null; then
+if type fd &>/dev/null; then
   export FZF_DEFAULT_COMMAND='fd --type file --hidden --follow --exclude .git --color=always'
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
   export FZF_ALT_C_COMMAND='fd --type directory --hidden --follow --exclude .git --color=always'
