15bbb46f86178e64e8e9b1e58545775aaa2dcc9c
LocTD <it.tranducloc@gmail.com>
2023-06-26 11:26:38 +0700

    [lazygit] Custom config

--- lazygit/install.sh
+++ lazygit/install.sh
@@ -7,4 +7,8 @@ curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/lates
 ls -l lazygit.tar.gz
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
-[ $? -ne 0] && echo "Failed to install lazygit - $(LAZYGIT_VERSION)"
+[ $? -ne 0] && echo "Failed to install lazygit - $(LAZYGIT_VERSION)" && exit 1
+/usr/local/bin/lazygit --version
+# https://github.com/jesseduffield/lazygit/blob/master/docs/Config.md
+ln -sf "$DOTFILES_ROOT/lazygit/config.yml" "$HOME/.config/lazygit/"
+cat "$HOME/.config/lazygit/config.yml"
