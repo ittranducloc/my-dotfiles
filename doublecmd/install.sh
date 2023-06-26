33bf5f6d2929ea0ef38e50ed2ef84a8c140cb42a
LocTD <it.tranducloc@gmail.com>
2023-06-26 12:11:27 +0700

    [doublecmd] Add installation script

---
+++ doublecmd/install.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+# https://launchpad.net/ubuntu/jammy/+package/doublecmd-common
+echo "=========Install Double Commander"
+echo ""
+apt show doublecmd-common
+sudo apt install -y doublecmd-common
