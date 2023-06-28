10d7f75742542e567597e13eb51dddb614727e13
LocTD <it.tranducloc@gmail.com>
2023-06-28 10:11:23 +0700

    [asdf] Fix go installation

--- asdf/go_install.sh
+++ asdf/go_install.sh
@@ -7,4 +7,4 @@ asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
 asdf install golang latest
 asdf global golang latest
 [ ! -x "$(command -v go)" ] && echo 'Fail to install go' && exit 1
-go --version
+go version
