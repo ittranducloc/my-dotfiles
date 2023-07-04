0252ccacf17d90cd6104b220efa53e799623bf49
LocTD <it.tranducloc@gmail.com>
2023-07-04 10:30:43 +0700

    [lazydocker] Config

--- lazydocker/install.sh
+++ lazydocker/install.sh
@@ -7,3 +7,8 @@ curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/i
 [ $? -ne 0 ] && echo "Failed to install lazydocker" && exit 1
 cd $CURRENT_DIR
 $HOME/.local/bin/lazydocker --version
+echo "Link config file"
+CONFIG_DIR=$HOME/.config/lazydocker
+mkdir -p "$CONFIG_DIR" 2>&1 >/dev/null
+ln -sf "$(pwd)/lazydocker/config.yml" "$CONFIG_DIR/config.yml"
+ls -l "$CONFIG_DIR/config.yml"
