de0cf1b48f5fbabddb9664c2eec87bbb97cf664a
LocTD <it.tranducloc@gmail.com>
2023-07-21 11:39:17 +0700

    [nerdfont] Install symbols

--- nerd-fonts/install.sh
+++ nerd-fonts/install.sh
@@ -5,6 +5,8 @@ cd /tmp
 echo "$(pwd)"
 curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Ubuntu.zip
 unzip -u ./Ubuntu.zip -d ~/.fonts
+culr -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/NerdFontsSymbolsOnly.zip
+unzip -u ./NerdFontsSymbolsOnly.zip -d ~/.fonts
 fc-cache -fv
 [ $? -ne 0 ] && echo 'Failed to install nerdfonts'
-rm -f ./Ubuntu.zip
+rm -f ./*.zip
