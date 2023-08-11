2c65667314beef6bc7d7b0669e3c67a5cd307c5f
LocTD <it.tranducloc@gmail.com>
2023-08-11 15:03:14 +0700

    [bypass-paywall-chrome-extension] Installation script

---
+++ bypass-paywalls-chrome-clean/install.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+# https://gitlab.com/magnolia1234/bypass-paywalls-chrome-clean
+echo "=========Install Bypass Paywall Chrome Extension"
+cd /tmp
+wget https://gitlab.com/magnolia1234/bypass-paywalls-chrome-clean/-/archive/master/bypass-paywalls-chrome-clean-master.zip
+unzip ./bypass-paywalls-chrome-clean-master.zip -d ~
+ls ~/bypass-paywalls-chrome-clean-master
+echo "Follow guidelines at https://gitlab.com/magnolia1234/bypass-paywalls-chrome-clean#load-unpacked-chrome-ms-edge-or-brave-all-desktop"
