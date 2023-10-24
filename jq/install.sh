390d5fe1541ff29d0fa9916f885dc4d402637eaa
LocTD <it.tranducloc@gmail.com>
2023-10-24 14:46:30 +0700

    [jq] Update installation script

--- jq/install.sh
+++ jq/install.sh
@@ -1,4 +1,6 @@
+#! /usr/bin/env bash
 echo '=========Install jq - json processor'
 [ ! d ~/bin ] && mkdir ~/bin
-curl -L https://github.com/jqlang/jq/releases/download/jq-1.6/jq-linux64 > ~/bin/jq
+curl -L https://github.com/jqlang/jq/releases/download/jq-1.6/jq-linux64 >~/bin/jq
 chmod +x ~/bin/jq
+~/bin/jq --version
