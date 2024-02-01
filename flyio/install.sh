006ea917bf7ab7cf6abeb31edb2db9463a06c2db
LocTD <it.tranducloc@gmail.com>
2024-02-01 12:52:10 +0700

    Install fly.io CLI

---
+++ flyio/install.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+echo "=========Install Fly.io CLI"
+curl -L https://fly.io/install.sh | sh
---
+++ flyio/path.zsh
@@ -0,0 +1,2 @@
+export FLYCTL_INSTALL="/home/loctd/.fly"
+export PATH="$FLYCTL_INSTALL/bin:$PATH"
