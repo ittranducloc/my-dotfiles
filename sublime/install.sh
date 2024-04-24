5290ca06ba73928060dc45ce5012f058df5c0170
LocTD <it.tranducloc@gmail.com>
2024-04-24 10:34:52 +0700

    update - Save sublime config

--- sublime/install.sh
+++ sublime/install.sh
@@ -1,5 +1,13 @@
 #! /usr/bin/env bash
+set -x
 echo "=========Install Sublime Text"
 snap search sublime-text
 sudo snap install --classic sublime-text
 snap info sublime-text
+if [ -d $HOME/.config/sublime-text/ ]; then
+	if [ ! -d $HOME/.config/sublime-text/Packages/ ]; then
+		mkdir "$HOME/.config/sublime-text/Packages/"
+	fi
+	ln -sf "$DOTFILES_DIR/sublime/User" "$HOME/.config/sublime-text/Packages/"
+fi
+set x
